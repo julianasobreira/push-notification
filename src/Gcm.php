d69c60487b8a49a303ea9c7e5f5a09554f3dad2c
Ladislau <caio.ladislau@sysvale.com>
2016-01-23 18:07:48 -0300

    Formata código conforme drupal coding standarts

--- src/Gcm.php
+++ src/Gcm.php
@@ -2,46 +2,42 @@
 
 namespace Sobreira\Gcm;
 
-class Gcm
-{
-    const URL = 'https://android.googleapis.com/gcm/send';
-    const GCM_API_KEY = "Authorization: key=xxxxx";
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function sendNote($title, $message, $registration_ids)
-    {        
-        $data = ["message" => $message, "title" => $title];
+class Gcm {
 
-        $fields = [
-            'registration_ids' => $registration_ids,
-            'data' => $data
-        ];
+  const URL = 'https://android.googleapis.com/gcm/send';
+  const GCM_API_KEY = "Authorization: key=xxxxx";
 
-        $headers = [
-            self::GCM_API_KEY,
-            'Content-Type: application/json'
-        ];
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function sendNote($title, $message, $registration_ids) {
+    $data = ["message" => $message, "title" => $title];
+    $fields = [
+      'registration_ids' => $registration_ids,
+      'data' => $data
+    ];
+    $headers = [
+      self::GCM_API_KEY,
+      'Content-Type: application/json'
+    ];
 
-        $ch = curl_init();
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, self::URL);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
 
-        curl_setopt($ch, CURLOPT_URL, self::URL);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-        
-        $result = curl_exec($ch);
-
-        if ($result === FALSE) {
-            die('Curl failed: ' . curl_error($ch));
-        } 
-        // Close connection
-        curl_close($ch);
-        
-        return $result;
+    if ($result === FALSE) {
+      die('Curl failed: ' . curl_error($ch));
     }
+    // Close connection
+    curl_close($ch);
+    return $result;
+  }
+
 }
